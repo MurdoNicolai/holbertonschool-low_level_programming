f2cd02d053c4544fbca164f1e2efef37628b02b6
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-07 18:13:57 +0200

    5.0 adding printf function

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/**
+ *main - prints sentance
+ *
+ *Return: Always 0
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+char str[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", str);
+return (0);
+}
