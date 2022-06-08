52b3a58f7a42f4b331718b0eb9f10735ca36312d
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-08 09:35:50 +0200

    adding the print 2 alphabets code

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - prints the alphabet
+*
+*Return: Always 0
+*/
+int main(void)
+{
+	int letter;
+	int newline = 10;
+
+	for (letter = 97; letter < 123; letter++)
+		putchar(letter);
+	for (letter = 65; letter < 91; letter++)
+		putchar(letter);
+	putchar(newline);
+}
