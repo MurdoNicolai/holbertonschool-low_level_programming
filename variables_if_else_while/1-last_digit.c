64312fca0c59a5dd8bf254c66f27bc417eb0255b
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-08 09:13:16 +0200

    1.0 adding The last digit  code

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - tells if random number is positive
+*
+*Return: Always 0
+*/
+int main(void)
+{
+	int n;
+	int lasn;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lasn = n % 10; /*put only the last digit of n into lasn*/
+	if (lasn > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, lasn);
+	}
+	else if (lasn == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, lasn);
+	}
+	else
+	{
+		printf("Last digit of");
+		printf("%d is %d and is less than 6 and not 0", n, lasn);
+	}
+	return (0);
+}
