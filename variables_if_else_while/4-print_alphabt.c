5e4636630184af543c377f5fcf7276aa18221997
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-08 09:41:30 +0200

    4.0 adding alphabet exept q/e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - prints the alphabet
+*
+*Return: Always 0
+*/
+int main(void)
+{
+	int letter;
+	int newline = 10;
+
+	for (letter = 97; letter < 123; letter++)
+	{
+		if (letter != 101 && letter != 113)
+			putchar(letter);
+	}
+	putchar(newline);
+}
