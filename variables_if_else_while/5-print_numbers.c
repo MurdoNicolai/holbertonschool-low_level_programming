bba75ac2b8f8b8d4e8556d4d346d801e0fde4b16
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-08 09:44:58 +0200

    5.0 adding print all the numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - prints numbers frome 0 to 9
+*
+*Return: Always 0
+*/
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d", number);
+	printf("\n");
+}
