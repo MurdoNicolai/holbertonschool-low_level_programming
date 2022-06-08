003392b995d2715b537a0a1e3fb83d002d5ab663
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-08 20:56:11 +0200

    8.0 adding hour counter

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+*jack_bauer - prints hours in a day
+*
+*Return: void
+*/
+void jack_bauer(void)
+{
+	/*init: print order "hour1number12" "number21number22", */
+	int hour1;
+	int hour2;
+	int minute1;
+	int minute2;
+
+	for (hour1 = 0; (hour1 < 2) || (hour2 < 4); hour1++)
+	{
+		for (hour2 = 0; ((hour1 < 2) || (hour2 < 4)) && (hour2 < 10); hour2++)
+		{
+			for (minute1 = 0; minute1 < 6; minute1++)
+			{
+				for (minute2 = 0; minute2 < 60; minute2++)
+				{
+					putchar(hour1);
+					putchar(hour2);
+					putchar(':');
+					putchar(minute1);
+					putchar(minute2);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
