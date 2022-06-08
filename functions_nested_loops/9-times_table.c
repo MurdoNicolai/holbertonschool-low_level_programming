a1db6a566feae3f97ecc076d87ad45ec59d80adf
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-08 21:26:13 +0200

    9.0 adding print times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+/**
+ *times_table - prints times table
+ *
+ *Return: void
+ */
+
+void times_table(void)
+{
+	int x;
+	int y;
+	int result;
+	/*we are printing result = x time y*/
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			result = x * y;
+			if (result < 10)
+				_putchar(result + '0');
+			else
+			{
+				_putchar((result - (result % 10)) / 10 + '0');
+				_putchar(result % 10 '0');
+				/*for 2 digit numbers we print each diget seperatly*/
+			}
+			if (x < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+				_putchar('\n');
+		}
+	}
