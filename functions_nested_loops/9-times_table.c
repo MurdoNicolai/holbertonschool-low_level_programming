7ba8da49d582580c5930d0eee0edd3e871d1f21b
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-08 21:32:12 +0200

    9.0.3 fixed presentation

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,7 +18,11 @@ void times_table(void)
 		{
 			result = x * y;
 			if (result < 10)
+			{
+				if (y != 0)
+					_putchar(' ');
 				_putchar(result + '0');
+			}
 			else
 			{
 				_putchar((result - (result % 10)) / 10 + '0');
@@ -28,7 +32,6 @@ void times_table(void)
 			if (x < 9)
 			{
 				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 			}
 			else
