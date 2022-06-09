e72f77ace679c168541f86a012b87a9d6975192d
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-09 12:43:35 +0200

    16.0 adding fibonaci till 98

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,47 @@
+/**
+ *main - prints first 50 fibonacci numbers
+ *
+ *Return: alway 0
+ */
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	int n;
+	long fibon = 2;
+	long fib = 1;
+	long fibon2;
+	long fib2;
+	long E15 = 1000000000000000;
+	int carryOver;
+
+	printf("%ld, %ld", fib, fibon);
+	for (n = 2; n < 88 ; n++)/*the first 2 ar already printed*/
+	{
+		fibon = fib + fibon;
+		fib = fibon - fib;
+		printf(", %ld", fibon);
+	}
+/*spliting numbers in 2 to reduce length*/
+	fibon2 = (fibon - (fibon % E15)) / E15;
+	fibon = fibon - (fibon2 * E15);
+	fib2 = (fib - (fib % E15)) / E15;
+	fib = fib - (fib2 * E15);
+	for (n = 88; n < 98; n++)/*to long to print simply*/
+	{
+		fibon = fib + fibon;
+		fib = fibon - fib;
+		carryOver = 0;
+		if (fibon > E15)
+		{
+			carryOver = 1;
+			fibon -= E15;
+		}
+		fibon2 = fib2 + fibon2 + carryOver;
+		fib2 = fibon2 - fib2 - carryOver;
+		printf(", %ld%ld", fibon2, fibon);
+	}
+	printf("\n");
+	return (0);
+}
