4b536454af91a3bd10a72e61709a1d554212d2d1
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-09 13:51:22 +0200

    updating main.h

--- debugging/main.h
+++ debugging/main.h
@@ -1,5 +1,7 @@
 #ifndef HEADER
 #define HEADER
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 int largest_number(int a, int b, int c);
 int positive_or_negative(int);
 #endif
