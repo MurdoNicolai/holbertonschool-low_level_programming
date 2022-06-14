e5c2ebc807e502221fb2ba865f5639f08daca7d0
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-14 10:50:42 +0200

    9.0 adding fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+/**
+ *main - prints numbers 1 to 100 replace multiples of 3/5 by fiz/buzz
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+		{
+			printf("Fizz");
+			if (n % 5 == 0)
+				printf(" ");
+		}
+		if (n % 5 == 0)
+			printf("Buzz ");
+		if (n % 15 != 0)
+			printf("%d ", n);
+	}
+	printf("\n");
+}
