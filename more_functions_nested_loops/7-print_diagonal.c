cd5473c9d45e9ad6511d226dfd3b1e1f99533ac1
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-14 10:34:22 +0200

    7.0 adding diagonal line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+/**
+ *print_diagonal - prints a diagonal line
+ *
+ *@n: length of line
+ *Return: void
+ */
+
+#include "main.h"
+
+void print_diagonal(int n)
+{
+	int lines;
+	int spaces;
+
+	for (lines = 0; lines < n ; lines++)
+	{
+		for (spaces = 1; spaces < lines; spaces++)
+			_putchar(20);
+		_putchar(92);
+		_putchar(10);
+	}
+}
