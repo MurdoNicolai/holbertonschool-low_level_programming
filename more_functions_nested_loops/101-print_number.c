1ebaf551604c442a2252afd85c8b10f482c35e13
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-14 12:30:38 +0200

    12.0.1 bugfix double to int again

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,7 @@
 void print_number(int n)
 {
 	int digit;
-	int pow;
+	int power;
 	int dvalue;
 
 	if (n < 0)
@@ -24,10 +24,10 @@ void print_number(int n)
 	{
 		for (digit = 10; digit >= 0; digit--)
 		{
-			pow = pow(10, digit);
-			if (n % pow != n)
+			power = pow(10, digit);
+			if (n % power != n)
 			{/*example: 2345678 and digit = 3*/
-				dvalue = n - (n % pow);/*1234000*/
+				dvalue = n - (n % power);/*1234000*/
 				dvalue /= pow(10, digit);/*1234*/
 				dvalue = dvalue % 10;/*4*/
 				_putchar(dvalue + '0');
