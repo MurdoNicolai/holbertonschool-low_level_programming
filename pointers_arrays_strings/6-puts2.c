5fd7b3efd6dda6e0f8fe44b680cc920079026030
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-14 15:45:54 +0200

    6.0 adding print every other caracter

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ *puts2 - prints every other caracter
+ *
+ *@str: string to print
+ *Return: void
+ */
+
+void puts2(char *str)
+{
+	int c;
+
+	for (c = 0; c < strlen(s) ; c += 2)
+		printf("%c", s[c]);
+	printf("\n");
+}
