6d94c1354391658b4bc0daba0ea3d093aa37ac3b
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-14 14:04:26 +0200

    4.0 adding print in reverse function

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ *print_rev - prints the reverse of string
+ *
+ *@s: string to reverse
+ *Return: void
+ */
+
+void print_rev(char *s)
+{
+	int c;
+
+	for (c = strlen(s); c >= 0 ; c--)
+		printf("%c", s[c]);
+}
