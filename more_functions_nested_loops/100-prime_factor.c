0832e324efd81911f00120b041bfddeb136dec44
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-14 11:55:53 +0200

    11.0.3 pleasing betty

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -1,8 +1,9 @@
 /**
  *main - prints largest prime factor of 612852475143
  *
- *Return - always 0
+ *Return: always 0
  */
+
 #include <stdio.h>
 
 int main(void)
@@ -12,7 +13,8 @@ int main(void)
 
 	while (n > 1)
 	{
-		for (factor = 2; (n % factor) != 0; factor++);
+		for (factor = 2; (n % factor) != 0; factor++)
+			;
 		n /= factor;
 	}
 	printf("%d\n", factor);
