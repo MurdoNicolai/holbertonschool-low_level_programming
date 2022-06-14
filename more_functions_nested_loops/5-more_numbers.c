34758f450260a91da9ae95363a90d678ccbcffa5
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-14 10:21:42 +0200

    5.1.1 bugfix unguarded for

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,11 @@ void more_numbers(void)
 	for (n = 0 ; n < 10 ; n++)
 	{
 		for (m = 0; m < 15; m++)
+		{
 			if (m >= 10)
 				_putchar('1');
 			_putchar((m % 10) + '0');
+		}
 		_putchar(10);
 	}
 }
