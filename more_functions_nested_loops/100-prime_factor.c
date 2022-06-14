edc289feb898fcfbf463ddc740b78b4a2f7d3c99
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-14 11:23:44 +0200

    11.0 adding prime factor

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+/**
+ *main - prints largest prime factor of 612852475143
+ *
+ *Return - always 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 2081;
+	int factor;
+
+	while (n > 1)
+	{
+		for (factor = 2; n % factor != 0; factor++)
+		{
+			n /= factor;
+		}
+	}
+	printf("%d\n", factor);
+	return (0);
+}
