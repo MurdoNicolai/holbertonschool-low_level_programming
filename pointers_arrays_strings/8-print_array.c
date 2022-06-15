0f41fd8396e37ac61b4f7c82d1d7a8816cc7f536
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-15 10:24:31 +0200

    8.0 adding print n elements

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ *print_array - prints n elements of aray
+ *
+ *@a: array to print
+ *@n: amount to print
+ *Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int c;
+	int len = n;
+
+	for (c = 0 ; c < len ; c++)
+		printf("%c", str[c]);
+	printf("\n");
+}
