3ee29ca94598de45ae86eb191671103147c8eda2
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-15 11:22:54 +0200

    10.0 adding atoi function

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,46 @@
+/**
+ *_atoi - converts string to char
+ *
+ *@s: string to convert
+ *Return: integer
+ */
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int _atoi(char *s)
+{
+	int len = strlen(s);
+	int c = 0;
+	int c2;
+	int i;
+	int power;
+	int add;
+	int integer = 0;
+	int ispositive = 1;
+
+	while (!isdigit(s[c]) || (s[c] != "\0"))
+	{
+		if (s[c] == '-')
+			ispositive--;
+		else if (s[c] == '+')
+			ispositive++;
+		c++;
+	}
+	c2 = c;
+	while (isdigit(s[c2]))
+	{
+		power++;
+	}
+	while (isdigit(s[c]))
+	{
+		add = (s[c] - '0');
+		for (i = 0; i < power; i++)
+			add *= 10;
+		power--;
+		integer += add;
+	}
+	if (ispositive <= 0)
+		integer *= -1;
+	return (integer);
+}
