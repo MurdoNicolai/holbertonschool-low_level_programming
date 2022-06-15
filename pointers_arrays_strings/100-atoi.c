781ce691491dba20a6d2da3aa15dd4f4a0c7605c
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-15 11:58:34 +0200

    10.0.1 bugfixes

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,6 +4,7 @@
  *@s: string to convert
  *Return: integer
  */
+
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -11,14 +12,14 @@
 int _atoi(char *s)
 {
 	int c = 0;
-	int c2;
-	int i;
-	int power;
-	int add;
+	int c2 = 0;
+	int i = 0;
+	int power = 0;
+	int add = 0;
 	int integer = 0;
 	int ispositive = 1;
 
-	while (!isdigit(s[c]) /*|| (s[c] != '\0')*/)
+	while (!isdigit(s[c]) && s[c])
 	{
 		if (s[c] == '-')
 			ispositive--;
@@ -27,17 +28,19 @@ int _atoi(char *s)
 		c++;
 	}
 	c2 = c;
-	while (isdigit(s[c2]))
+	while (isdigit(s[c2]) && s[c])
 	{
 		power++;
+		c2++;
 	}
-	while (isdigit(s[c]))
+	while (isdigit(s[c]) && s[c])
 	{
 		add = (s[c] - '0');
-		for (i = 0; i < power; i++)
+		for (i = 1; i < power; i++)
 			add *= 10;
 		power--;
 		integer += add;
+		c++;
 	}
 	if (ispositive <= 0)
 		integer *= -1;
