05c9396083a622659b0d0d3d2215fe3d61ce3f3e
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-16 13:27:07 +0200

    6.0 adding capitalising words

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ **string_toupper - upercase all char in string
+ *
+ *@str: str to upper
+ *Return: new string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+	int w;
+	int len = strlen(str);
+	int word = 1;
+	int seperator[13] = {9, 10, 20, 21, 22, 40, 41, 44, 46, 59, 63, 123, 125};
+
+	for (i = 0; i <= len ; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z' && word == 1)
+		{
+			str[i] = str[i] - 32;
+			word = 0;
+		}
+		w = 0;
+		while (word == 0 || w < 13)
+		{
+			if (str[i] == seperator[w])
+				word = 1;
+			w++;
+		}
+	}
+	return (str);
+}
