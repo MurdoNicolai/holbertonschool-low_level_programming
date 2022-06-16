a05d0b15214538724b17d30f44fa497f1f748b04
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-16 15:05:37 +0200

    9.0 adding print number

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+/**
+ *print_number - prints integer
+ *
+ *@n: integer to print
+ *Return: void
+ */
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+
+void print_number(int n)
+{
+	int i = 1000000000;
+	int car;
+
+	while (i > n && n > 0)
+		i /= 10;
+
+	while (n > 0)
+	{
+		car = ((n - (n % i)) / i) + 48;
+		n =  n % i;
+		printf("%c", car);
+		i /= 10;
+	}
+}
