640d5fafff46a0d56b7983b8138e33fb76299e23
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-16 11:24:11 +0200

    2.0 adding strcp

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ **_strncpy - copy a string
+ *
+ *@dest: destination
+ *@src: source
+ *@n: number of digits
+ *Return: void
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int len = strlen(src);
+
+	for (i = 0; (i <= len) && (i < n); i++)
+		dest[i] = src[i];
+	return (dest);
+}
