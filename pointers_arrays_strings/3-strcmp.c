1f7322680a6b6fa1ad6b16f3a8dc20bd26da596d
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-16 11:41:54 +0200

    3.0 adding strcompare

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ **_strcmp - compare 2 strings
+ *
+ *@s1: first string
+ *@s2: string to compare to
+ *Return: void
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+
+	for (i = 0; i <= len1 ; i++)
+		sum1 += src[i];
+	for (i = 0; i <= len2 ; i++)
+		sum2 += src[i];
+	return (sum1 - sum2);
+}
