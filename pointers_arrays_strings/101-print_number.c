5421aff4b2d709c504dca08637d6300fc477abe1
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-16 18:08:47 +0200

    9.0.2 bugfix negatives

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -14,15 +14,28 @@ void print_number(int n)
 {
 	int i = 1000000000;
 	int car;
+	int incr = 0;
 
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n < -i)
+		{
+			n++;
+			posit = 1;
+		}
+		n *= -1;
+	}
 	while (i > n && n > 0)
 		i /= 10;
 
-	while (n > 0)
+	while (n > 9)
 	{
 		car = ((n - (n % i)) / i) + 48;
 		n =  n % i;
 		_putchar(car);
 		i /= 10;
 	}
+
+	_putchar(n + '0' + posit);
 }
