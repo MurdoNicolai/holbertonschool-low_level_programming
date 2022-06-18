e16ca3f1e1a3e6d68641d12f65700d3bc0f01b7e
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-18 18:17:50 +0200

    12.0.1 deleting putchar from file

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -12,11 +12,11 @@
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
- */
+ *
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
-}
+	}*/
 
 /**
  *print_hexa - prints hexadecimal valu of int
