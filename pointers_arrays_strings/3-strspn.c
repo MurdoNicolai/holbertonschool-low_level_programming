0b7a3d0b0588e6a3dbd1d64145e5681f66ecd742
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-19 20:50:37 +0200

    3.0 adding count string fction

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+/**
+ *_strspn - counts bytes in one string 1 till 1 not present in string 2
+ *
+ *@s: string to test
+ *@accept: bytes to test for s
+ *Return: the amount
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int n;
+	int i;
+
+	for (n = 0; s[n]; n++)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (s[n] != accept[i])
+				return (n);
+		}
+	}
+}
