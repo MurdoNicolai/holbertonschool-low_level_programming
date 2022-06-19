93da738996362160cb3c90406ca6dee06e6c36fd
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-19 21:18:22 +0200

    5.0 adding find word fnctn

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+#include <stdio.h>
+/**
+ *_strpbrk - finds first occurance of word in string
+ *
+ *@haystack: string to test
+ *@needle: word to find
+ *Return: location of needle
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int n;
+	int i;
+	int test;
+
+	for (n = 0; haystack[n]; n++)
+	{
+		test = 0;
+		for (i = 0; needle[i] && test != -1; i++)
+		{
+			if (haystack[n + i] == accept[i])
+				test = 1;
+			else
+				test = -1;
+		}
+		if (test == 1)
+			return (haystack + n);
+	}
+	return (NULL);
+}
