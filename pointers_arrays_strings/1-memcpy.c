8e6a4a557e719f269fac622cd0d1ff2f81fa7619
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-19 20:14:55 +0200

    1.0.2 small bugfixes

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (src[i] && dext[i])
+		if (src[i] && dest[i])
 			src[i] = dest[i];
+	}
 	return (src);
 }
