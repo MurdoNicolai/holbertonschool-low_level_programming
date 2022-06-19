5e19ac904bdb1913065bfdec814e1edd51274559
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-19 20:55:40 +0200

    3.1 changing location of return and adding test

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,14 +10,18 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int n;
 	int i;
+	int test;
 
 	for (n = 0; s[n]; n++)
 	{
+		test = 0;
 		for (i = 0; accept[i]; i++)
 		{
-			if (s[n] != accept[i])
-				return (n);
+			if (s[n] == accept[i])
+				test = 1;
 		}
+		if (test == 0)
+			return (n);
 	}
 	return (n);
 }
