d4d0393a03f1d6988addd05e39d2c5212ce70d7f
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-19 19:58:52 +0200

    0.0 adding filling with byte

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+/**
+ *_memset - fils memory with constant byte
+ *
+ *@n: amount to bytes to fill
+ *@s: starting point
+ *@b: fill with this
+ *Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
