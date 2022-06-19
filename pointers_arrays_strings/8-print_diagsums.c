19faec68b4defeecbf4c2730dd7a146ecf1eee6f
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-19 21:51:06 +0200

    8.0.2 using 1 dim array not 2

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -16,8 +16,8 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i][i];
-		sum2 += a[size - i][i];
+		sum1 += a[i + (size * i)];
+		sum2 += a[size - i + (size * i)];
 	}
-	printf("%s, %s\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
