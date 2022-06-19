48ac8526d06a63d87aed47f36365fe15eee1cd89
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-19 21:40:48 +0200

    8.0 adding print sum diag

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+/**
+ *print_diagsums - ints both sums of diagonals of square matrix
+ *
+ *@a: matrix to sum
+ *@size: size of a
+ *Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i][i];
+		sum2 += a[size - i][i];
+	}
+	printf("%s, %s\n", sum1, sum2);
+}
