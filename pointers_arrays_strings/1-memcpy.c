a7b2ce473c8890a1347f2c2867ab2f89cdd8ab9f
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-19 20:21:17 +0200

    1.0.4 replacing return

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,9 +12,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
-		if (src[i] && dest[i])
-			dest[i] = src[i];
-	}
-	return (src);
+		dest[i] = src[i];
+	return (dest);
 }
