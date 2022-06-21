aa4f0bfaf494730af815437d3c7d799050c7f265
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-21 11:26:05 +0200

    1.0 adding print in reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+/**
+ *_print_rev_recursion - prints in reverse
+ *
+ *@s: string to print
+ *Return: void
+ */
+
+
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar(10);
+}
