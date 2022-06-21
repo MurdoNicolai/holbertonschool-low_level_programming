94e530af094282efb56e634b2ac9bcb2ddf42f4c
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-21 15:02:08 +0200

    8.1 rplace check all cracter for a check for *

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -4,19 +4,19 @@
 /**
  *compare - compares 2 strings
  *
- *@n1: caracter being tested frome S1
- *@n2: caracter being tested frome s2
+ *@n1: caracter being tested frome S1 "0"
+ *@n2: caracter being tested frome s2 "0"
  *@s1: string 1
  *@s2: string 2 can contain * standing for any caracter
- *@c: optional caracter
+ *@c: check for * "0"
  *Return: 1 if identical, 2 otherwise
  */
 
-int compare(char *s1, char *s2, int n1, int n2, char c)
+int compare(char *s1, char *s2, int n1, int n2, int c)
 {
 	if (!(s2[n2]))
 	{
-		if (c == '*')
+		if (c == 1)
 			return (1);
 		if (!(s1[n1]))
 			return (1);
@@ -25,24 +25,24 @@ int compare(char *s1, char *s2, int n1, int n2, char c)
 
 	if (s2[n2] == '*')
 	{
-		return (compare(s1, s2, n1, n2 + 1, '*'));
+		return (compare(s1, s2, n1, n2 + 1, 1));
 	}
 
 	if (s1[n1] != s2[n2])
 	{
-		if (c == 7)
+		if (c == 0)
 			return (0);
-		if (c == '*')
-			return (compare(s1, s2, n1 + 1, n2, '*'));
-		return (compare(s1, s2, n1, n2 + 1, 7));
+		if (c == 1)
+			return (compare(s1, s2, n1 + 1, n2, 1));
 	}
 
-	if (c == 7)
-		return (compare(s1, s2, n1 + 1, n2 + 1, 7));
-	return (compare(s1, s2, n1 + 1, n2, s2[n2]));
+	if (c == 1)
+	{
+		if (compare(s1, s2, n1 + 1, n2, 1))
+			return (1);
+	}
+	return (compare(s1, s2, n1 + 1, n2 + 1, 0));
 }
-
-
 
 
 
@@ -61,5 +61,5 @@ int compare(char *s1, char *s2, int n1, int n2, char c)
 
 int wildcmp(char *s1, char *s2)
 {
-	return (compare(s1, s2, 0, 0, 7));
+	return (compare(s1, s2, 0, 0, 0));
 }
