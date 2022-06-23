7a51da717075388a83adff922bd132b1543e4ba8
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-23 14:56:30 +0200

    3.0.2 rong output fixed

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -18,21 +18,25 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = (int **) malloc(width * sizeof(int));
+	array = (int**) malloc(width * sizeof(int));
 
 	if (!array)
+	{
+		free(array);
 		return (NULL);
+	}
 	for (i = 0; i < width; i++)
 	{
-		array[i] = malloc(height * sizeof(int));
+		array[i] =(int*) malloc(height * sizeof(int));
 		if (!array[i])
+		{
+			free(array);
 			return (NULL);
+		}
 	}
-	if (!array)
-		return (NULL);
-	for (i = 0; i <= height; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j <= width; j++)
+		for (j = 0; j < width; j++)
 		{
 			array[i][j] = 0;
 		}
