71fa3c341abc1e23f2fba40d4c6216e3dbec5563
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-23 11:30:30 +0200

    0.0.1 pleasing betty

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ *main - prints name of exectuable
+ *
+ *@argc: useless
+ *@argv: contains name
+ *Return: 0
+ */
 int main(int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
