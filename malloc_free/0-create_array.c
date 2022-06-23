1535b9b4ceecde4dec0d6b4bc121ab41ee2ebd5e
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-23 13:27:36 +0200

    0.0.0 pleasing betty

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,11 +12,12 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char* array;
+	char *array;
 	unsigned int i;
 
-	if (size == 0 || !(array = (char*) malloc(size * sizeof(char))))
-		return NULL;
+	array = (char *) malloc(size * sizeof(char));
+	if (size == 0 || !(array))
+		return (NULL);
 	for (i = 0; i < size; i++)
 		array[i] = c;
 	return (array);
