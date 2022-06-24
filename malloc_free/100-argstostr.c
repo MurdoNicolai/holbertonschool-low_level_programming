fed4f122732ac5c452a500abcb0519fd4425b062
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-24 12:04:58 +0200

    5.0 adding concatenate args

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ *argstostr - concatenates all arguments into a single string
+ *
+ *@av: array of arguments
+ *@ac: number of arguments
+ *Return: pointer to array or NULL if fail
+ */
+
+
+
+char *argstostr(int ac, char **av)
+{
+	char *array;
+	unsigned int size = 0;
+	int i;
+	unsigned int j;
+
+	array = (char *) malloc(size * sizeof(char));
+	if (!av || ac == 0)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		size += strlen(av[i]) + 1;
+		array = (char *) realloc(array, size * sizeof(char));
+		if (!array)
+			return (NULL);
+		for (j = 0; j <= strlen(av[i]); j++)
+		{
+			array[size - strlen(av[i]) + j - 1] = av[i][j];
+			if (j == strlen(av[i]))
+				array[size - 1] = 10;
+		}
+	}
+	array = (char *) realloc(array, (size + 1) * sizeof(char));
+	array[size] = 0;
+	return (array);
+}
