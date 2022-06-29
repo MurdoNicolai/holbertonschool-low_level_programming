bee22c14fe8f16d26801d2234344ad11e3c0e048
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-29 11:29:23 +0200

    3.0 adding array gen

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+ *array_range - return an array of numbers betwin min and max
+ *
+ *@min: smallest number
+ *@max: largest number
+ *Return: pointer to the array
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *array;
+
+	if (min > max)
+		return (NULL);
+	array = malloc ((max - min + 1) * sizeof(int));
+	if (!array)
+		return (NULL);
+	for (i = min; i <= max; i++)
+		array[i - min] = i;
+	return (array);
+}
