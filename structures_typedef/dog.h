7ce7a33c1b6551103e9fb79e6c12d17572e8a175
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-29 15:17:15 +0200

    0.0.2 pleasing betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+/**
+ * struct dog - dog description
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ *
+ * Description: basic structure to describe a dog
+ */
 struct dog
 {
 	char *name;
