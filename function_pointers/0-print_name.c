0dd338e7d53f1318b11cab8898cfdb31655a08f3
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-30 16:34:26 +0200

    0.0 adding basic function of function

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *print_name -  prints name using function
+ *
+ *@name: name to print
+ *@f: function to use
+ *Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
