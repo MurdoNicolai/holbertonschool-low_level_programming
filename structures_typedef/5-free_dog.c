52b71312cda535b83f88dab869ea691d4872e132
Murdo <murdo.nicolai.pro@gmail.com>
2022-06-30 15:30:17 +0200

    5.0 adding free dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ *free_dog - frees a variable of type dog
+ *
+ *@d: pointer to free
+ *Return: void
+ */
+void free_dog(dog_t *d);
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
