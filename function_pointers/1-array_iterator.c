9e0e99310d85705c4e09bcdf6bf698f9a94acdc3
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-01 11:20:38 +0200

    1.0 adding function to use function several times

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *array_iterator - uses a function on each element of array
+ *@array: contains all inputs to function
+ *@size: array length
+ *@action: function to use
+ *Return: void
+ */
+
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!action)
+		exit(1);
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
