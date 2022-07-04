e6b053d6ea347ab31e099679414de68fd3032109
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-04 14:41:32 +0200

    1.0.1 typo

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -6,20 +6,20 @@
  *print_numbers - prints numbers seperated by seperator
  *
  *@n: amount of numbers to print
- *@seperator: seperator between numbers
+ *@separator: seperator between numbers
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
-	int i;
+	unsigned int i;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d ", va_arg(ap, int));
-		if (!seperator || i != n - 1)
-			printf("%s", seperator);
+		if (!separator || i != n - 1)
+			printf("%s", separator);
 	}
 	va_end(ap);
 	putchar('\n');
