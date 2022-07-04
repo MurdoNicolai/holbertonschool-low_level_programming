1784485e8414db1702742f41008fa7209b0d595f
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-04 14:39:58 +0200

    1.0 printing numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *print_numbers - prints numbers seperated by seperator
+ *
+ *@n: amount of numbers to print
+ *@seperator: seperator between numbers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", va_arg(ap, int));
+		if (!seperator || i != n - 1)
+			printf("%s", seperator);
+	}
+	va_end(ap);
+	putchar('\n');
+}
