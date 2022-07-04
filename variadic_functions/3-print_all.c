ac03a800b915c16ccda27b4f9b28afcc4c2a5fcf
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-04 15:41:31 +0200

    3.0 adding print anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include "variadic_functions.h"
+
+/**
+ *print_all - prints list in any format
+ *
+ *@format: list of formats to print
+ *Return: void
+ */
+
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	char *toprint;
+	char *form;
+
+	va_start(ap, n);
+	for (i = 0; i < strlen(format); i++)
+	{
+		form = format[i];
+		if (strchr("cifs", form))
+		{
+			toprint = "%";
+			strncat(toprint, form, 1);
+			if (form == "s")
+				printf("(nil)");
+			else
+				printf(toprint, va_arg(ap, char*));
+		}
+	}
+	va_end(ap);
+	putchar('\n');
