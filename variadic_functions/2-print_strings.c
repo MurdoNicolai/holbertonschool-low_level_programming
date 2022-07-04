44755bd4e391c6b7948e2ede603da84c3de15218
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-04 14:53:07 +0200

    2.0.2 changeing condition on arguments printing

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		word = va_arg(ap, char*);
-		if (!word)
+		if (word != NULL)
 			printf("%s", word);
 		else
 			printf("(nil)");
