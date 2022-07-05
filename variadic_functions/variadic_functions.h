53406c3fc9f7c13898643ef46c0b77538807ac4f
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-05 16:15:17 +0200

    3.0.5 adding .h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,7 +2,7 @@
 #define HEADER
 
 #define getvalu(X)  ((X) > (0) ? (printf(toprint, va_arg(ap, int))) : printf(toprint, (va_arg(ap, double))))
-#define start(X) ((X) < (1) ? (void) 0 : (va_start(ap, format)))
+#define start(X) ((X) > (0) ? (va_start(ap, format)) : (void) 0)
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
