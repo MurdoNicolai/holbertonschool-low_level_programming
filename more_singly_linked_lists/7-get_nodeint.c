8edf21e79bf15e764932a5eca82fc640adc79e8c
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-17 23:47:50 +0200

    add get

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -21,7 +21,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (head != NULL)
 	{
 		if (count == index)
-			return (h);
+			return (head);
 		head = head->next;
 		count++;
 	}
