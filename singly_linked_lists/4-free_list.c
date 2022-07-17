1f2e9a3922584bed6a426f3bc31575310f004bd4
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-17 23:01:44 +0200

    add node

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -11,12 +11,13 @@
 
 void free_list(list_t *head)
 {
-   list_t *tmp;
+	list_t *tmp;
 
-   while (head != NULL)
+	while (head != NULL)
 	{
-	   tmp = head;
-	   head = head->next;
-	   free(tmp);
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
 	}
 }
