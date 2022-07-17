c846f2106ecf793c533286c04afc8caf23bb9361
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-17 23:27:17 +0200

    add print

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *pop_listintn - delete head
+ *
+ *@head: headlist delete
+ *Return: value
+ */
+
+int pop_listint(listint_t **head)
+{
+	int r = *head->n;
+	listint_t *tmp;
+
+	if (!head)
+		return (0);
+	tmp = *head;
+	*head = *head->next;
+	free tmp;
+	return (r);
+}
