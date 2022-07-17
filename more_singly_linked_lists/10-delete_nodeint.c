201b835a17e4e10ca3f21facd42a8d78cddceb5d
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-18 00:39:49 +0200

    add sum

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			tmptmp = tmp->next;
 			tmp->next = tmptmp->next;
-			free (tmptmp);
+			free(tmptmp);
 			return (1);
 		}
 		tmp = tmp->next;
