ae25b39522b07fe55863544e4ca97197702e9205
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-17 23:14:30 +0200

    add print

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -13,7 +13,7 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *newNode = malloc(sizeof(list_t));
+	listint_t *newNode = malloc(sizeof(listint_t));
 
 	if (newNode == NULL)
 	{
