31a856514d1b29b997ef7b3065b3ff22db96bb81
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-17 22:07:17 +0200

    add nil for null strings

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,11 +15,14 @@ size_t print_list(const list_t *h)
 
 	if (h == NULL)
 	{
-		printf("[0] (nil)");
 		return (0);
 	}
 	while (h != NULL)
 	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
