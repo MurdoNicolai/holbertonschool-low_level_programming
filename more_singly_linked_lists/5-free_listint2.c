153283804a242aabb7df3e6318a7a470421967df
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-17 23:20:07 +0200

    add print

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -3,20 +3,21 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *free_listint - frees a list
+ *free_listint2 - frees a list
  *@head: head of list
  *Return: void
  */
 
 
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head);
 {
 	listint_t *tmp;
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
-		tmp = head;
-		head = head->next;
+		tmp = *head;
+		*head = *head->next;
 		free(tmp);
 	}
+	*head = NULL;
 }
