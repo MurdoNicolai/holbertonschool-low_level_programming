f69a9f77c2574b8fa359c936ea61360f503670d4
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-17 22:04:19 +0200

    add inlcudes

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,10 +1,13 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
 /**
  *print_list - prints a list
  *
  *@h: list to print
  *Return: amount of nodes printed
  */
-
 
 size_t print_list(const list_t *h)
 {
