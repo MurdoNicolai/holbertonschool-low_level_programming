f922c4f5619a45d3cb1db30e5ddc3d7d8c2fbfcb
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-17 22:09:10 +0200

    bugfix nilprint

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -23,8 +23,11 @@ size_t print_list(const list_t *h)
 		{
 			printf("[0] (nil)\n");
 		}
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
 		count++;
 	}
 	return (count);
