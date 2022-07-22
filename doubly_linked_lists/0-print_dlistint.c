517dea42990972d2f8acb0de01c326f4154871f6
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-22 19:25:33 +0200

    0.0 adding print list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *print_dlistint_t - prints a list
+ *
+ *@h: list to print
+ *Return: amount of nodes printed
+ */
+
+size_t print_dlistint_t(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
