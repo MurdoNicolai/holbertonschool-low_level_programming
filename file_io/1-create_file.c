ec3e51480959f7e3217e36fbdb8b07ea391c9cc8
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-22 17:39:41 +0200

    1.1 using creat instead of open

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,10 +12,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int make; 
 	FILE *new;
 
-	make = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	new = fopen(filename, "w");
 	if (!filename || !new)
 		return (-1);
