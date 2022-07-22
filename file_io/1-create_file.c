76a2ef6768a2fc4f9c3866914fdb7e2eb085f13c
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-22 16:52:32 +0200

    1.0 add create new file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename || !new)
 		return (-1);
-	fprintf(new, text_content);
+	fprintf(new, "%s", text_content);
 	fclose(new);
 	return (1);
 }
