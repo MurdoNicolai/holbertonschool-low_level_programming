9bf25511d9cb01d623d968d50e38d17eb59fdd26
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-22 16:51:24 +0200

    1.0 add create new file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ *create_file - creates a file
+ *@filename: name the file will have
+ *@text_content: what will be in the file
+ *Return: 1 or -1
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	FILE *new = fopen(filename, "w+");
+
+	if (!filename || !new)
+		return (-1);
+	fprintf(new, text_content);
+	fclose(new);
+	return (1);
+}
