35026b3618db413f4d2d388594455d8213479a6f
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-22 12:23:39 +0200

    0.0.1 doing it without using %

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,13 +9,13 @@
 
 void print_binary(unsigned long int n)
 {
-        int i = 23;
+	int i = 23;
 
-        for(i = 31; n == (n & (0xffffffff >> (32 - i))) && i > 0; i--)
-                n = (n & (0xffffffff >> (32 - i)));
-        for(; i >= 0; i--)
-        {
-                printf("%ld",(n >> i));
-                n = (n & (0xffffffff >> (32 - i)));
-        }
+	for (i = 31; n == (n & (0xffffffff >> (32 - i))) && i > 0; i--)
+		n = (n & (0xffffffff >> (32 - i)));
+	for (; i >= 0; i--)
+	{
+		_putchar((n >> i) + '0');
+		n = (n & (0xffffffff >> (32 - i)));
+	}
 }
