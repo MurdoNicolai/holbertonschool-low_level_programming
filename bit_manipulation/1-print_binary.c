a2e61e27f4aeafa281ced837b9e11f44519f3cec
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-22 12:19:40 +0200

    1.0 adding print binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *print_binary - prints binary representation of number
+ *@n: number to print
+ *Return: void
+ */
+
+
+void print_binary(unsigned long int n)
+{
+        int i = 23;
+
+        for(i = 31; n == (n & (0xffffffff >> (32 - i))) && i > 0; i--)
+                n = (n & (0xffffffff >> (32 - i)));
+        for(; i >= 0; i--)
+        {
+                printf("%ld",(n >> i));
+                n = (n & (0xffffffff >> (32 - i)));
+        }
+}
