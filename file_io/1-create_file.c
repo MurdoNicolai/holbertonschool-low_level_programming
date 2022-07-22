c71c53d1d844efe1299914d9a8c55b06a95f618a
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-22 17:02:12 +0200

    1.0.1 change permition on file creation

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	FILE *new = fopen(filename, "0600");
+	FILE *new = fopen(filename, 0600);
 
 	if (!filename || !new)
 		return (-1);
