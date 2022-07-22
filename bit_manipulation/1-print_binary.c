72a2d2d6cb65fe5fd32c328bf5490a1865ee53e8
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-22 12:26:36 +0200

    0.0.2 doing ti for 64 bites not 32

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,13 +9,13 @@
 
 void print_binary(unsigned long int n)
 {
-	int i = 23;
+	int i;
 
-	for (i = 31; n == (n & (0xffffffff >> (32 - i))) && i > 0; i--)
-		n = (n & (0xffffffff >> (32 - i)));
+	for (i = 63; n == (n & (0xffffffffffffffff >> (64 - i))) && i > 0; i--)
+		n = (n & (0xffffffffffffffff >> (64 - i)));
 	for (; i >= 0; i--)
 	{
 		putchar((n >> i) + '0');
-		n = (n & (0xffffffff >> (32 - i)));
+		n = (n & (0xffffffffffffffff >> (64 - i)));
 	}
 }
