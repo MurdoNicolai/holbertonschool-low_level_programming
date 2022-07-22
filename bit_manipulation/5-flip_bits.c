17960bb13d15339746baa67f1da8a3d40b3b9c1e
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-22 14:01:09 +0200

    5.0 adding how many bits to flip?

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,35 @@
+/**
+ *get_bit - gts a bit at given index
+ *@n: number to check
+ *@index: bit to get
+ *Return: the value
+ */
+
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= 64)
+		return (-1);
+	return ((n >> index) % 2);
+}
+
+
+/**
+ * flip_bits - how many bitflips to change 1 number to another?
+ * @n: first number
+ * @m: second number
+ * Return: amount of flips
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i;
+	int res = 0;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (get_bit(n ^ m, i))
+			res++;
+	}
+	return (res);
+}
