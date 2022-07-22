a8fac6d443c3764305fc9b068eae2908569ddcf2
Murdo <murdo.nicolai.pro@gmail.com>
2022-07-22 17:25:36 +0200

    1.1 using creat instead of open

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,8 +12,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	FILE *new = creat(filename, 0600);
+	FILE *new; 
 
+	new = creat(filename, 0600);
 	if (!filename || !new)
 		return (-1);
 	if (!text_content)
