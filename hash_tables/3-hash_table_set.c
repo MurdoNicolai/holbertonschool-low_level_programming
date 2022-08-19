2ba31ffec7f19f9cf5f4cac98a1225c81b86560e
murdonicolai <murdo.nicolai.pro@gmail.com>
2022-08-19 11:46:23 +0200

    3.0.2 replacing chain instead of adding

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,13 +16,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int keyID = key_index((unsigned char *)key, ht->size);
 	hash_node_t *new = malloc(sizeof(hash_node_t));
 
-	if (!new)
-		return (0);
-	new->value = malloc(sizeof((char *)value));
-	strcpy(new->value,(char *)value);
 	new->key = malloc(sizeof((char *)key));
+	new->value = malloc(sizeof((char *)value));
+	if (!(new) || !(new->key) || !(new->value))
+		return (0);
+	strcpy(new->value,(char *)value);
 	strcpy(new->key,(char *)key);
-	if (!ht->array[keyID])
+	if (1/*!ht->array[keyID]*/)
 	{
 		new->next = NULL;
 		ht->array[keyID] = new;
