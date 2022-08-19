5adec232abe61ed66797c2f0e6e6e454ea20b84c
murdonicolai <murdo.nicolai.pro@gmail.com>
2022-08-19 12:56:04 +0200

    5.0.2 small pres fix

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -12,6 +12,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *p;
+	int d = 0;
 
 	printf("{");
 	for (i = 0; ht && i < ht->size; i++)
@@ -19,9 +20,11 @@ void hash_table_print(const hash_table_t *ht)
 		p = ht->array[i];
 		while (p)
 		{
-			printf(", ");
+			if (d)
+				printf(", ");
 			printf("'%s': '%s'", p->key, p->value);
 			p = p->next;
+			d = 1;
 		}
 	}
 	printf("}\n");
