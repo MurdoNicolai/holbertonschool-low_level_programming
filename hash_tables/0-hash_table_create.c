efe0efeb4e892893bcedcf2d33bbca691a6b3828
murdonicolai <murdo.nicolai.pro@gmail.com>
2022-08-19 09:57:20 +0200

    0.1 pleasing betty and putting pointers in table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,10 +3,16 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - create an empty hash table
+ * @size: size of table to create
+ * Return: NULL or the hash table
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new = malloc(sizeof(hash_table_t));
-	hash_node_t **Narray = malloc(sizeof(hash_node_t) * size);
+	hash_node_t **Narray = malloc(sizeof(*hash_node_t) * size);
 
 	if (!new || !Narray)
 		return (NULL);
