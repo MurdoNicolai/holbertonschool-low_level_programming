55ef8147817d0b8fe51293c59c965cd57e07f1a7
murdonicolai <murdo.nicolai.pro@gmail.com>
2022-08-19 12:41:36 +0200

    4.0 adding retrieve value

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves the value assosiated with key
+ * @ht: hashtable to retrieve frome
+ * @key: key to use
+ * Return: the founc value ot NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp;
+
+	if (!ht)
+		return (0);
+	tmp = ht->array[key_index((unsigned char *)key , ht->size)];
+	for (; tmp; tmp = tmp->next)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+	}
+	return (NULL);
+}
