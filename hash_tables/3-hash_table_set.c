c2f025c1770877bdaded1deed61f8e93468fa4cf
murdonicolai <murdo.nicolai.pro@gmail.com>
2022-08-19 12:16:59 +0200

    3.0.3 replacing only when the key is identical

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,20 +14,40 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int keyID = key_index((unsigned char *)key, ht->size);
-	hash_node_t *new = malloc(sizeof(hash_node_t));
+	hash_node_t *new;
+	hash_node_t *tmp;
 
-	new->key = malloc(sizeof((char *)key));
-	new->value = malloc(sizeof((char *)value));
-	if (!(new) || !(new->key) || !(new->value))
-		return (0);
-	strcpy(new->value,(char *)value);
-	strcpy(new->key,(char *)key);
-	if (1/*!ht->array[keyID]*/)
+	if (!ht->array[keyID])
 	{
+		new  = malloc(sizeof(hash_node_t));
+		new->key = malloc(sizeof((char *)key));
+		new->value = malloc(sizeof((char *)value));
+		if (!(new) || !(new->key) || !(new->value))
+			return (0);
+		strcpy(new->value, (char *)value);
+		strcpy(new->key, (char *)key);
 		new->next = NULL;
 		ht->array[keyID] = new;
 		return (1);
 	}
+	for (tmp = ht->array[keyID]; tmp; tmp = tmp->next)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			tmp->value = realloc(tmp->value, sizeof((char *) value));
+			if (!(tmp->value))
+				return (0);
+			strcpy(tmp->value, (char *)value);
+			return (1);
+		}
+	}
+	new  = malloc(sizeof(hash_node_t));
+	new->key = malloc(sizeof((char *)key));
+	new->value = malloc(sizeof((char *)value));
+	if (!(new) || !(new->key) || !(new->value))
+		return (0);
+	strcpy(new->value, (char *)value);
+	strcpy(new->key, (char *)key);
 	new->next = ht->array[keyID];
 	ht->array[keyID] = new;
 	return (1);
