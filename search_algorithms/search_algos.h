a9bac8817f0e9c504e36d3e6d6744f7237019577
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-08-24 16:55:32 +0200

    updating header

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -2,5 +2,7 @@
 #define HEADER_FILE
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
