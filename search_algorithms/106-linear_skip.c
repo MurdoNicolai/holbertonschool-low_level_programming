2d5332219b59d8c51f5d8400ac8fcb794a85e547
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-08-25 14:18:55 +0200

    13.0 adding list skip with express rout

--- search_algorithms/106-linear_skip.c
+++ search_algorithms/106-linear_skip.c
@@ -4,35 +4,28 @@
 #include "search_algos.h"
 
 /**
-*jump_list - find node using jumpsearch algo
+*linear_skip - find node using jumpsearch algo
 *@list: list to search
-*@size: size of array
 *@value: value to find
 *Return: pointer to node or -1
 **/
 
-listint_t *jump_list(listint_t *list, size_t size, int value)
+skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	int k = sqrt(size);
-	int i;
-	listint_t *tmp = list;
+	skiplist_t *tmp = list;
 
-	if(!list || !size)
+	if (!list)
 		return (NULL);
 	while (list)
 	{
-		for (i = 0; i < k && list->next	; i++)
-			list =  list->next;
+		list =  list->express;
 		printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
-		if (list->n >= value || !(list->next))
+		if (list->n >= value || !(list->express))
 			break;
 		tmp = list;
 	}
-	if (k != 0)
-	{
-		printf("Value found between indexes ");
-		printf("[%ld] and [%ld]\n", tmp->index, list->index);
-	}
+	printf("Value found between indexes ");
+	printf("[%ld] and [%ld]\n", tmp->index, list->index);
 	for (list = tmp; list ; list =  list->next)
 	{
 		printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
